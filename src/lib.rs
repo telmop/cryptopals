f4b66b44b8ff12e83f8a0d95f18f0af78556775a
Telmo <tjppires@gmail.com>
2025-03-25 00:05:58 +0000

    Add base64 decode method.

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ fn byte_to_b64ascii(byte: u8) -> u8 {
 }
 
 pub fn b64encode(bytes: &[u8]) -> Result<String> {
-    let mut encoded = vec![];
+    let mut encoded = vec![]; // TODO: Switch to with_capacity.
     let mut buf: u8 = 0;
     for (i, b) in bytes.iter().enumerate() {
         let is_last = i == bytes.len() - 1;
@@ -44,6 +44,66 @@ pub fn b64encode(bytes: &[u8]) -> Result<String> {
         }
     }
     Ok(String::from_utf8(encoded)?)
+}
+
+fn b64ascii_to_byte(ascii: u8) -> (u8, bool) {
+    // Returns the respective value for each base64 character.
+    // The boolean describes whether it is a padding value ('=') or not.
+    match ascii {
+        b'=' => (0, true),
+        b'A'..=b'Z' => (ascii - b'A', false),
+        b'a'..=b'z' => (ascii - b'a' + 26, false),
+        b'0'..=b'9' => (ascii - b'0' + 52, false),
+        b'+' => (62, false),
+        b'/' => (63, false),
+        _ => panic!("This can't happen"),
+    }
+}
+
+pub fn b64decode(encoded: &str) -> Result<Vec<u8>> {
+    // 1110_1101 0010_1001 0000_1111
+    // 111011 01_0010 1001_00 001111
+    let mut buf: u8 = 0;
+    let mut decoded: Vec<u8> = vec![]; // TODO: Switch to with_capacity.
+    for (i, b) in encoded.as_bytes().into_iter().enumerate() {
+        match i % 4 {
+            0 => {
+                let (value, is_padding) = b64ascii_to_byte(*b);
+                if is_padding {
+                    // Can't have padding at the beginning of a cycle.
+                    return Err("Invalid base64 string".into());
+                }
+                buf = value << 2;
+            }
+            1 => {
+                let (value, is_padding) = b64ascii_to_byte(*b);
+                if is_padding {
+                    // Can't have padding at this stage of the cycle.
+                    return Err("Invalid base64 string".into());
+                }
+                decoded.push(buf + ((value & 0b110000) >> 4));
+                buf = (value & 0b1111) << 4;
+            }
+            2 => {
+                let (value, is_padding) = b64ascii_to_byte(*b);
+                if is_padding {
+                    break;
+                }
+                decoded.push(buf + ((value & 0b111100) >> 2));
+                buf = (value & 0b11) << 6;
+            }
+            3 => {
+                let (value, is_padding) = b64ascii_to_byte(*b);
+                if is_padding {
+                    break;
+                }
+                decoded.push(buf + value);
+                buf = 0;
+            },
+            _ => panic!("Impossible"),
+        }
+    }
+    Ok(decoded)
 }
 
 pub fn hexstr_to_bytes(hex: &str) -> Result<Vec<u8>> {
